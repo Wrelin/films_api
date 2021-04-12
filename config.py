d08bbcfb66b3bf7c822722c74982ea00796ee163
Wrell <wrell@yandex.ru>
2021-04-12 23:25:11 +0300

    postgres

--- config.py
+++ config.py
@@ -1,9 +1,10 @@
+import os
 import pathlib
 
 BASE_DIR = pathlib.Path(__file__).parent
 
 
 class Config(object):
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
+    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or 'sqlite:///' + str(BASE_DIR / "data" / "db.sqlite3")
     SQLALCHEMY_TRACK_MODIFICATIONS = False
     SECRET_KEY = 'you-will-newer-know'
